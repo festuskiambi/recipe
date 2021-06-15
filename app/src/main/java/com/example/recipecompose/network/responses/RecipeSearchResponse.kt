e7a72eaf07c1aec0b6ce22188bd589bccfd2ea19
Festus Kiambi <festuskiambi@Festuss-MacBook-Pro.local>
2021-06-15 12:20:29 +0300

    create recipe retrofit interface.
    Signed-off-by: Festus Kiambi <festuskiambi@Festuss-MacBook-Pro.local>

---
+++ app/src/main/java/com/example/recipecompose/network/responses/RecipeSearchResponse.kt
@@ -0,0 +1,13 @@
+package com.example.recipecompose.network.responses
+
+import com.example.recipecompose.network.model.RecipeNetworkEntity
+import com.google.gson.annotations.SerializedName
+
+class RecipeSearchResponse(
+
+    @SerializedName("count")
+    var count: Int,
+
+    @SerializedName("results")
+    var recipes: List<RecipeNetworkEntity>,
+)
